f91b91498ec33c0ec1e4eae66d6ad117d088ede4
jorge pilcante <jorgeapd5@hotmail.com>
2021-08-18 11:00:59 -0400

    watchlist1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-    validates :title, :overview, presence: :true  
+    validates :title, :overview, presence: :true, uniqueness: :true  
     
-    has_many :bookmarks, dependent: :destroy
+    has_many :bookmarks
 end
